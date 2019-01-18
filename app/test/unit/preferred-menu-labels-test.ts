bb6fde18a2017eaed63a709fbb6e8d0e74495561
Brendan Forster <github@brendanforster.com>
2019-01-18 16:12:38 -0400

    writing some more tests

--- app/test/unit/preferred-menu-labels-test.ts
+++ app/test/unit/preferred-menu-labels-test.ts
@@ -1,4 +1,7 @@
 import { getPreferredLabels } from '../../src/lib/preferred-menu-labels'
+import { Shell as MacOSShell } from '../../src/lib/shells/darwin'
+import { Shell as WindowShell } from '../../src/lib/shells/win32'
+import { Shell as LinuxShell } from '../../src/lib/shells/linux'
 
 describe('getPreferredLabels', () => {
   const baseParameters = {
@@ -26,6 +29,49 @@ describe('getPreferredLabels', () => {
       })
 
       expect(actual.removeRepositoryLabel).toBe(expectedLabelRequireConfirm)
+    })
+  })
+
+  describe('editorLabel', () => {
+    const baseParameters = {
+      currentPullRequest: null,
+      defaultBranch: null,
+      showRepositoryRemoveDialog: false,
+    }
+
+    it('returns undefined by default', () => {
+      const actual = getPreferredLabels({
+        ...baseParameters,
+      })
+
+      expect(actual.shellLabel).toBeUndefined()
+    })
+
+    it('can render a macOS entry', () => {
+      const actual = getPreferredLabels({
+        ...baseParameters,
+        selectedShell: MacOSShell.iTerm2,
+      })
+
+      expect(actual.shellLabel).toBe('Open in iTerm2')
+    })
+
+    it('can render a Windows shell', () => {
+      const actual = getPreferredLabels({
+        ...baseParameters,
+        selectedShell: WindowShell.GitBash,
+      })
+
+      expect(actual.shellLabel).toBe('Open in Git Bash')
+    })
+
+    it('can render a Windows shell', () => {
+      const actual = getPreferredLabels({
+        ...baseParameters,
+        selectedShell: LinuxShell.Konsole,
+      })
+
+      expect(actual.shellLabel).toBe('Open in Konsole')
     })
   })
 })
